62cd0936e0fab59536f8ac1d6b083659672b76be
npcrites <nicholascrites22@gmail.com>
2023-03-23 22:59:14 -0700

    committing homework pre-GH linter

--- containers/BST.py
+++ containers/BST.py
@@ -1,6 +1,7 @@
 '''
 This file implements the Binary Search Tree data structure.
-The functions in this file are considerably harder than the functions in the BinaryTree file.
+The functions in this file are considerably harder than the
+functions in the BinaryTree file.
 '''
 
 from containers.BinaryTree import BinaryTree, Node
@@ -9,7 +10,8 @@ from containers.BinaryTree import BinaryTree, Node
 class BST(BinaryTree):
     '''
     The BST is a superclass of BinaryTree.
-    That means that the BST class "inherits" all of the methods from BinaryTree,
+    That means that the BST class "inherits" all of
+    the methods from BinaryTree,
     and we don't have to reimplement them.
     '''
 
@@ -20,19 +22,25 @@ class BST(BinaryTree):
         then each element of xs needs to be inserted into the BST.
         '''
         super().__init__()
+        self.root = None
+        if xs is not None:
+            for element in xs:
+                self.insert(element)
 
     def __repr__(self):
         '''
         Notice that in the BinaryTree class,
         we defined a __str__ function,
         but not a __repr__ function.
-        Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
+        Recall that the __repr__ function should return a string that can
+        be used to recreate a valid instance of the class.
         Thus, if you create a variable using the command BST([1,2,3])
         it's __repr__ will return "BST([1,2,3])"
 
         For the BST, type(self).__name__ will be the string "BST",
         but for the AVLTree, this expression will be "AVLTree".
-        Using this expression ensures that all subclasses of BST will have a correct implementation of __repr__,
+        Using this expression ensures that all subclasses of BST will have
+        a correct implementation of __repr__,
         and that they won't have to reimplement it.
         '''
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
@@ -40,9 +48,11 @@ class BST(BinaryTree):
     def is_bst_satisfied(self):
         '''
         Whenever you implement a data structure,
-        the first thing to do is to implement a function that checks whether
+        the first thing to do is to implement a function
+        that checks whether
         the structure obeys all of its laws.
-        This makes it possible to automatically test whether insert/delete functions
+        This makes it possible to automatically test whether
+        insert/delete functions
         are actually working.
         '''
         if self.root:
@@ -54,22 +64,29 @@ class BST(BinaryTree):
         '''
         FIXME:
         The current implementation has a bug:
-        it only checks if the children of the current node are less than/greater than,
-        rather than ensuring that all nodes to the left/right are less than/greater than.
+        it only checks if the children of the current
+        node are less than/greater than,
+        rather than ensuring that all nodes to the
+        left/right are less than/greater than.
 
         HINT:
         Use the _find_smallest and _find_largest functions to fix the bug.
-        You should use the _ prefixed methods because those are static methods just like this one.
+        You should use the _ prefixed methods because
+        those are static methods just like this one.
         '''
         ret = True
         if node.left:
             if node.value >= node.left.value:
                 ret &= BST._is_bst_satisfied(node.left)
+                if BST._find_largest(node.left) > node.value:
+                    ret = False
             else:
                 ret = False
         if node.right:
             if node.value <= node.right.value:
                 ret &= BST._is_bst_satisfied(node.right)
+                if BST._find_smallest(node.right) < node.value:
+                    ret = False
             else:
                 ret = False
         return ret
@@ -82,8 +99,23 @@ class BST(BinaryTree):
         Implement this function.
 
         HINT:
-        Create a staticmethod helper function following the pattern of _is_bst_satisfied.
+        Create a staticmethod helper function following
+        the pattern of _is_bst_satisfied.
         '''
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            BST._insert(self.root, value)
+
+    @staticmethod
+    def _insert(node, value):
+        if node is None:
+            return Node(value)
+        if value < node.value:
+            node.left = BST._insert(node.left, value)
+        else:
+            node.right = BST._insert(node.right, value)
+        return node
 
     def insert_list(self, xs):
         '''
@@ -94,8 +126,11 @@ class BST(BinaryTree):
 
         HINT:
         Repeatedly call the insert method.
-        You cannot get this method to work correctly until you have gotten insert to work correctly.
+        You cannot get this method to work correctly
+        until you have gotten insert to work correctly.
         '''
+        for element in xs:
+            self.insert(element)
 
     def __contains__(self, value):
         '''
@@ -110,6 +145,7 @@ class BST(BinaryTree):
         FIXME:
         Implement this function.
         '''
+        return BST._find(value, self.root)
 
     @staticmethod
     def _find(value, node):
@@ -117,6 +153,14 @@ class BST(BinaryTree):
         FIXME:
         Implement this function.
         '''
+        if node is None:
+            return False
+        if node.value == value:
+            return True
+        if node.value > value:
+            return BST._find(value, node.left)
+        else:
+            return BST._find(value, node.right)
 
     def find_smallest(self):
         '''
@@ -130,7 +174,8 @@ class BST(BinaryTree):
     @staticmethod
     def _find_smallest(node):
         '''
-        This is a helper function for find_smallest and not intended to be called directly by the user.
+        This is a helper function for find_smallest and
+        not intended to be called directly by the user.
         '''
         assert node is not None
         if node.left is None:
@@ -148,6 +193,18 @@ class BST(BinaryTree):
         HINT:
         Follow the pattern of the _find_smallest function.
         '''
+        if self.root is None:
+            raise ValueError('Nothing in tree')
+        else:
+            return BST._find_largest(self.root)
+
+    @staticmethod
+    def _find_largest(node):
+        assert node is not None
+        if node.right is None:
+            return node.value
+        else:
+            return BST._find_largest(node.right)
 
     def remove(self, value):
         '''
@@ -158,11 +215,53 @@ class BST(BinaryTree):
         Implement this function.
 
         HINT:
-        You should have everything else working before you implement this function.
+        You should have everything else working
+        before you implement this function.
 
         HINT:
         Use a recursive helper function.
         '''
+        if self.root is not None:
+            if value == self.root.value:
+                if self.root.left is None:
+                    if self.root.right is None:
+                        self.root = None
+        else:
+            BST._remove(value, self.root)
+        print("post deleting " + str(value))
+        print(self.root)
+
+    @staticmethod
+    def _remove(value, node):
+        if node is None:
+            return node
+        if value < node.value:
+            node.left = BST._remove(value, node.left)
+        elif value > node.value:
+            node.right = BST._remove(value, node.right)
+        else:
+            # node with one child or none
+            if node.left is None and node.right is None:
+                node = None
+                return node
+            elif node.left is None:
+                temp = node.right
+                node.value = node.right.value
+                node.right = node.right.right
+                # node.right = None
+                return temp
+            elif node.right is None:
+                temp = node.left
+                node.value = node.left.value
+                node.left = node.left.left
+                # node.left = None
+                return temp
+            # node with two children, largest right sbt successor
+            else:
+                temp = BST._find_smallest(node.right)
+                node.value = temp
+                node.right = BST._remove(temp, node.right)
+        return node
 
     def remove_list(self, xs):
         '''
@@ -174,3 +273,22 @@ class BST(BinaryTree):
         HINT:
         See the insert_list function.
         '''
+        for element in xs:
+            self.remove(element)
+
+    def __iter__(self):
+        return BSTIter(self.inorder(self.root, ""))
+
+
+class BSTIter:
+    def __init__(self, xs):
+        self.xs = xs
+        self.i = 0
+
+    def __next__(self):
+        if self.i >= len(self.xs):
+            raise StopIteration
+        else:
+            result = self.xs[self.i]
+            self.i += 1
+            return result
--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -1,14 +1,17 @@
 '''
 This file implements the Node and BinaryTree classes.
-These two classes are the building blocks for the BST, AVLTree, and Heap data structures.
-It is crucial to get these implemented correctly in order to be able to implement the other data structures.
+These two classes are the building blocks for the BST, AVLTree,
+and Heap data structures.
+It is crucial to get these implemented correctly in order
+to be able to implement the other data structures.
 '''
 
 
 class Node():
     '''
     You do not have to implement anything within this class.
-    Given a node t, you can visualize the node by running str(t) in the python interpreter.
+    Given a node t, you can visualize the node by running str(t)
+    in the python interpreter.
     This is a key method to perform debugging,
     so you should get familiar with how to visualize these strings.
     '''
@@ -45,7 +48,8 @@ class BinaryTree():
     def __init__(self, root=None):
         '''
         Construct a BinaryTree, possibly with a single element in it.
-        Note that for an ordinary BinaryTree, we cannot insert more than one element in the constructor,
+        Note that for an ordinary BinaryTree, we cannot insert
+        more than one element in the constructor,
         but for the BST (and other tree types) we can.
         '''
         if root:
@@ -61,8 +65,11 @@ class BinaryTree():
 
     def print_tree(self, traversal_type):
         '''
-        There are three primary types of tree traversals: preorder, inorder, and postorder.
-        All three of these traversals are implemented for you as a reference on how to write recursive functions on recursive data structures.
+        There are three primary types of tree traversals:
+        preorder, inorder, and postorder.
+        All three of these traversals are implemented for you as
+        a reference on how to write recursive functions on recursive
+        data structures.
         '''
         if traversal_type == 'preorder':
             return self.preorder_print(self.root, '')
@@ -71,7 +78,8 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder_print(self.root, '')
         else:
-            raise ValueError('Traversal type ' + str(traversal_type) + ' is not supported.')
+            raise ValueError('Traversal type ' +
+                             str(traversal_type) + ' is not supported.')
 
     def preorder_print(self, start, traversal):
         '''
@@ -109,35 +117,66 @@ class BinaryTree():
         but instead of printing the tree,
         it returns the contents of the tree as a list.
 
-        A general programming principle is that a function should return its results
+        A general programming principle is that a function
+        should return its results
         rather than print them whenever possible.
         If a function returns its results,
-        we can always print the returned results if we need to visualize them.
-        But by returning the results we can also do more computations on the results if needed.
-        Many of the test cases for more complicated tree functions rely on this to_list function,
+        we can always print the returned results if we
+        need to visualize them.
+        But by returning the results we can also do more
+        computations on the results if needed.
+        Many of the test cases for more complicated tree
+        functions rely on this to_list function,
         so it is import to implement it correctly.
 
         FIXME:
         Implement this function by modifying the _print functions above.
         '''
+        if traversal_type == 'preorder':
+            return self.preorder(self.root, '')
+        elif traversal_type == 'inorder':
+            return self.inorder(self.root, '')
+        elif traversal_type == 'postorder':
+            return self.postorder(self.root, '')
+        else:
+            raise ValueError('Traversal type ' + str(traversal_type) +
+                             ' is not supported.')
 
     def preorder(self, start, traversal):
         '''
         FIXME:
         Implement this function by modifying the _print functions above.
         '''
+        if start is not None:
+            return ([start.value]
+                    + self.preorder(start.left, '')
+                    + self.preorder(start.right, ''))
+        else:
+            return []
 
     def inorder(self, start, traversal):
         '''
         FIXME:
         Implement this function by modifying the _print functions above.
         '''
+        if start is not None:
+            return (self.inorder(start.left, '')
+                    + [start.value]
+                    + self.inorder(start.right, ''))
+        else:
+            return []
 
     def postorder(self, start, traversal):
         '''
         FIXME:
         Implement this function by modifying the _print functions above.
         '''
+        if start is not None:
+            return (self.postorder(start.left, '')
+                    + self.postorder(start.right, '')
+                    + [start.value])
+        else:
+            return []
 
     def __len__(self):
         '''
@@ -155,15 +194,31 @@ class BinaryTree():
         HINT:
         The pseudocode is:
         add 1 for the current node;
-        if a left child exists, add the result of __len__helper on the left child;
-        if a right child exists, add the result of __len__helper on the right child;
+        if a left child exists, add the result of __len__helper
+        on the left child;
+        if a right child exists, add the result
+        of __len__helper on the right child;
         return the sum of these three steps
         '''
+        if node is not None:
+            z = 1
+        else:
+            return 0
+        if node.left is not None:
+            x = BinaryTree.__len__helper(node.left)
+        else:
+            x = 0
+        if node.right is not None:
+            y = BinaryTree.__len__helper(node.right)
+        else:
+            y = 0
+        return z + x + y
 
     def height(self):
         '''
         Returns the height of the tree.
-        Recall that the height is the maximum length from the root to a leaf node.
+        Recall that the height is the maximum length from
+        the root to a leaf node.
 
         FIXME:
         Implement this function.
@@ -171,6 +226,7 @@ class BinaryTree():
         HINT:
         See how the __len__ method calls its helper staticmethod.
         '''
+        return BinaryTree._height(self.root)
 
     @staticmethod
     def _height(node):
@@ -182,5 +238,16 @@ class BinaryTree():
         The pseudocode is:
         if a left child exists, calculate the _height of the left child;
         if a right child exists, calculate the _height of the right child;
-        return 1 (for the current node) plus the max of the left and right _heights calculated above
+        return 1 (for the current node) plus the max of the
+        left and right _heights calculated above
         '''
+        if node is None:
+            return -1
+        else:
+            left_depth = BinaryTree._height(node.left)
+            right_depth = BinaryTree._height(node.right)
+
+            if left_depth > right_depth:
+                return left_depth + 1
+            else:
+                return right_depth + 1
